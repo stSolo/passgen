abc14b806714dc6226ba2b9867933bc405916a1d
Stanislav Solovyev <st.solp@gmail.com>
2021-09-04 16:13:34 +0300

    Add raw code

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub const AZ: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+pub const az: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+pub const nums: &[u8] = b"0123456789";
+pub const symbs: &[u8] = b")(*&^%$#@!~-_+";
